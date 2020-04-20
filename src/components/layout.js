502a6f21233f5198e6ea8e45f683e4aa58133b87
Robert Winterstein <rrwinterstein@gmail.com>
2020-04-20 12:04:49 -0400

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "./layout.css"
+
+const Layout = ({ children }) => (
+  <div>
+    <main>{children}</main>
+  </div>
+)
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+import "../styles/global.scss"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="" />
+    <h1>test</h1>
+  </Layout>
+)
+
+export default IndexPage
