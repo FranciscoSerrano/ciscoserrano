6aec60668ca0baffac4807c43c62f704a4b7d8bb
Robert Winterstein <rrwinterstein@gmail.com>
2020-04-22 23:21:49 -0400

    Begins adding dark-mode functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,47 @@
 import React, { Component } from "react"
 import styled from "styled-components"
 
-// Import any Components
-import SEO from "../components/seo" // SEO is only necessary for rendered pages
-import Buttons from "../components/buttons"
-import Hero from "../components/hero"
-
 // Import all Stylesheets
 import "../styles/layout.css"
 import "../styles/colors.css"
 import "../styles/global.scss"
 
-const Layout = styled.main``
+// Import any Components
+import SEO from "../components/seo" // SEO is only necessary for rendered pages
+import ThemeSwticher from "../components/themeSwitcher"
+import Buttons from "../components/buttons"
+import Hero from "../components/hero"
+
+const Layout = styled.main`
+  &.dark-mode {
+    background-color: var(--black);
+
+    * {
+      color: var(--white);
+    }
+  }
+
+  &.light-mode {
+    background-color: var(--white);
+    * {
+      color: var(--black);
+    }
+  }
+`
 
 export default class index extends Component {
+  state = {
+    dark: false,
+  }
+
   render() {
     return (
-      <Layout>
+      <Layout className={this.state.dark ? "dark-mode" : "light-mode"}>
         <SEO title="Francisco Serrano" />
         <Buttons>
+          <div onClick={() => this.setState({ dark: !this.state.dark })}>
+            <ThemeSwticher />
+          </div>
         </Buttons>
         <Hero />
       </Layout>
